e8cd5f5b45be1c67ba297bb3d0809f55c4a28e1f
echowaney <1194165366@example.com>
2017-01-29 23:01:26 +0800

    处理头布局的三种状态 ,自己好好看,这三种的状态的条件 1, 判断条件是 recycleview的第一个条目的下标为0 并且y方向的偏移量大于0 2. 状态的切换:　判断依据　是由当前状态是下拉刷新状态　并且呢下拉变化的高度ｄｉｓ　是否等于０ 来进行释放刷新的切换

--- app/src/main/java/com/example/hashwaney/zhbj33/view/CostumeRecycleview.java
+++ app/src/main/java/com/example/hashwaney/zhbj33/view/CostumeRecycleview.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -34,16 +37,25 @@ public class CostumeRecycleview
     TextView     mTvRefresh;
     @BindView(R.id.tv_time)
     TextView     mTvTime;
+    @BindView(R.id.iv_arrow)
+    ImageView    mIvArrow;
     @BindView(R.id.ll_refresh)
     LinearLayout mLlRefresh;
-    //    @BindView(R.id.progress_load_more)
-    ProgressBar  mProgressLoadMore;
+
     //    @BindView(R.id.ll_loadmore)
     LinearLayout mLlLoadmore;
+
     private ViewGroup mHeadView;
     private View      mFootView;
     private int       mMHeadMeasuredHeight;
     private int       mMFootMeasuredHeight;
+    private              int HeadState             = DOWN_REFRESH_STATE;
+    private final static int DOWN_REFRESH_STATE    = 0;
+    private final static int RELEASE_REFRESH_STATE = 1;
+    private final static int REFRESHING_STATE      = 2;
+    private Animation mDownAnimation;
+    private Animation mUpAnimation;
+    private float mDisY;
 
 
     public CostumeRecycleview(Context context) {
@@ -66,8 +78,46 @@ public class CostumeRecycleview
         initHeadView();
         //初始化脚布局
         initFootView();
+        //创建动画
+        initAnimation();
 
 
+    }
+
+    private void initAnimation() {
+        mDownAnimation = creatDownAnimation();
+        mUpAnimation = creatUpAnimation();
+
+
+    }
+
+    private Animation creatDownAnimation() {
+        // -180 到 -360
+        RotateAnimation rotateAnimation = new RotateAnimation(-180,
+                                                              -360,
+                                                              Animation.RELATIVE_TO_SELF,
+                                                              0.5f,
+                                                              Animation.RELATIVE_TO_SELF,
+                                                              0.5f);
+        rotateAnimation.setDuration(1000);
+        rotateAnimation.setFillAfter(true);
+
+
+        return rotateAnimation;
+    }
+
+    private Animation creatUpAnimation() {
+        //0 - -180
+        RotateAnimation rotateAnimation = new RotateAnimation(0,
+                                                              -180,
+                                                              Animation.RELATIVE_TO_SELF,
+                                                              0.5f,
+                                                              Animation.RELATIVE_TO_SELF,
+                                                              0.5f);
+        rotateAnimation.setDuration(1000);
+        rotateAnimation.setFillAfter(true); //维持状态不变
+
+        return rotateAnimation;
     }
 
     private void initFootView() {
@@ -84,6 +134,8 @@ public class CostumeRecycleview
     private void initHeadView() {
         mHeadView = (ViewGroup) inflate(getContext(), R.layout.head_view, null);
         ButterKnife.bind(this, mHeadView);
+        //隐藏进度条
+        mPbRefresh.setVisibility(INVISIBLE);
         //隐藏头布局
         mLlRefresh.measure(0, 0);
         mMHeadMeasuredHeight = mLlRefresh.getMeasuredHeight();
@@ -109,6 +161,7 @@ public class CostumeRecycleview
     //这里采用是dispathontouchevent ,因为dispathtouch 使用的频率更高
 
     private LinearLayoutManager lm;
+
     @Override
     public void setLayoutManager(LayoutManager layout) {
         super.setLayoutManager(layout);
@@ -118,37 +171,73 @@ public class CostumeRecycleview
 
     private float startX;
     private float startY;
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                Log.e(TAG, "dispatchToucEvent:  按下" );
+                Log.e(TAG, "dispatchToucEvent:  按下");
                 startX = ev.getX();
-                startY=ev.getY();
+                startY = ev.getY();
                 break;
 
             case MotionEvent.ACTION_MOVE:
-                Log.e(TAG, "dispatchTouchEvent:  移动" );
-                float moveX =ev.getX();
+                Log.e(TAG, "dispatchTouchEvent:  移动");
+                float moveX = ev.getX();
 
-                float moveY =ev.getY();
+                float moveY = ev.getY();
                 //条件 是recycleview的第一个条目的角标为0  并且 向下移动
-                float disY = moveY -startY;
-                int dis = (int) (-mMHeadMeasuredHeight +disY);
+                mDisY = moveY - startY;
+                int dis = (int) (-mMHeadMeasuredHeight + mDisY);
                 int firstVisibleItemPosition = lm.findFirstVisibleItemPosition();
-                if (firstVisibleItemPosition==0  && disY>0){
+                if (firstVisibleItemPosition == 0 && mDisY > 0) {
+                    //当前状态是下拉刷新 并且 dis ==0 了, 就切换到释放刷新
+                    if ( HeadState == DOWN_REFRESH_STATE    && dis == 0) {
+                        HeadState = RELEASE_REFRESH_STATE;
+                        //修改文字
+                        mTvRefresh.setText("释放刷新");
+                        //动画 0 - -180 旋转动画
+                        mIvArrow.startAnimation(mUpAnimation);
 
-                    mLlRefresh.setPadding(0,dis,0,0);
+
+                    } else if (HeadState == RELEASE_REFRESH_STATE && dis < 0) {
+                        //当前状态是释放刷新 并且dis<0 就切换到下拉刷新
+                        HeadState = DOWN_REFRESH_STATE;
+                        mTvRefresh.setText("下拉刷新");
+                        mIvArrow.startAnimation(mDownAnimation);
+                    }
+
+                    mLlRefresh.setPadding(0, dis, 0, 0);
 
                 }
 
                 break;
             case MotionEvent.ACTION_UP:
-                Log.e(TAG, "dispatchTouchEvent:  弹起" );
+                Log.e(TAG, "dispatchTouchEvent:  弹起");
+                //处理条件 : 条目角标同样也是为0 ,并且y方向的距离大于0 有向下移动
+                int visibleItemPosition = lm.findFirstVisibleItemPosition();
+                if (visibleItemPosition==0 && mDisY >0){
+                    if (HeadState == DOWN_REFRESH_STATE){
+                        //直接让他不显示
+                        mLlRefresh.setPadding(0,-mMHeadMeasuredHeight,0,0);
+                    }else if (HeadState ==RELEASE_REFRESH_STATE){
+                        //更改状态
+                        HeadState = REFRESHING_STATE;
+                        //隐藏箭头 ,清除动画
+                        mIvArrow.clearAnimation();
+                        mIvArrow.setVisibility(INVISIBLE);
+                        //显示进度条
+                        mPbRefresh.setVisibility(VISIBLE);
+                        //更改文字
+                        mTvRefresh.setText("加载更多数据...");
+                        //停留在
+                        mLlRefresh.setPadding(0,0,0,0);
+                        //加载数据 TODO
+                    }
+                }
+
                 break;
         }
-
-
         return super.dispatchTouchEvent(ev);
     }
 }
